f8404a06c22fc251900441fc0144b8ddb773c758
Mateusz Waligóra <mat.waligora@gmail.com>
2019-11-28 21:37:40 +0100

    Hidden nav and added h1

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+<!--    <div id="nav">-->
+<!--      <router-link to="/">Home</router-link> |-->
+<!--      <router-link to="/about">About</router-link>-->
+<!--    </div>-->
+      <h1>Strategos</h1>
     <router-view />
   </div>
 </template>
